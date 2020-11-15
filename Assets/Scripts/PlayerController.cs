24788c2c594a5650f590c7c6e6438516c33c4bbf
cmlee0913 <43628924+cmlee0913@users.noreply.github.com>
2020-11-16 04:11:39 +0900

    11.16

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody playerRigid;
+    public GameObject player;
+    public float speed;
+    public float maxSpeed;
+    public float moveHorizontal;
+    public float moveVertical;
+    public Vector3 movement;
+    public Vector3 playerVector3;
+
+    void Start() {
+
+    }
+
+    void FixedUpdate() {
+
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        moveVertical = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        playerRigid.AddForce(movement*speed);
+
+    }
+}
