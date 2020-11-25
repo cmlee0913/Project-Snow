f94b1479990986adcdc49b98ba44e5d2eec49eb5
cmlee0913 <43628924+cmlee0913@users.noreply.github.com>
2020-11-26 04:37:13 +0900

    11.26
    complete camera rotate

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    public new GameObject camera;
-    public GameObject player;
-    public GameObject cameraSpot;
+    [SerializeField]
+    private new GameObject camera;
+    private GameObject player;
+    private GameObject cameraSpot;
+    private Rigidbody playerRigid;
+    private float mouseX;
+    private float mouseY;
+    private float x;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
         camera = GameObject.FindWithTag("Camera");
         cameraSpot = GameObject.FindWithTag("CameraSpot");
+        playerRigid = player.GetComponent<Rigidbody> ();
     }
 
     private void Update()
@@ -23,7 +29,7 @@ public class Controller : MonoBehaviour
 
     private void FixedUpdate()
     {
-
+        Move();
     }
 
     private void Balance()
@@ -38,20 +44,32 @@ public class Controller : MonoBehaviour
 
     private void Camera()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
         Vector3 camAngle = cameraSpot.transform.rotation.eulerAngles;
-        float x = camAngle.x - mouseY;
+        x = camAngle.x - mouseY;
 
-        if(x < 180f)
+        if (x < 180f)
         {
             x = Mathf.Clamp(x, -1f, 70f);
         }
-        else
+        else if (180f <= x)
         {
             x = Mathf.Clamp(x, 335f, 361f);
         }
 
         cameraSpot.transform.rotation = Quaternion.Euler(x, camAngle.y + mouseX, camAngle.z);
     }
+
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        float speed = 10;
+
+        playerRigid.velocity = new Vector3(cameraSpot.transform.forward.x,
+                                           0,
+                                           cameraSpot.transform.forward.z).normalized * speed;
+    }
 }
+// Reference https://www.youtube.com/watch?v=P4qyRyQdySw&t=510s
